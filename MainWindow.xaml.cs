d4294049b8b83169077269371993602ef76f4ba8
hunor64 <nhunor06@gmail.com>
2025-04-04 08:24:31 +0200

    8. Feladat kész

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -24,25 +24,8 @@ namespace VIZSGA_KOROM
             InitializeComponent();
             ReadFile("autok.csv");
 
-            /// 5. Feladat
-            lblMain.Content += $"5. Feladat: {Cars.Count} autó található a listában.\n";
+            ConsoleFeladat();
 
-            /// 6. Feladat
-            double osszesEladas = Cars.Select(x => x.EladottDarabszam).Average();
-            lblMain.Content += $"6. Feladat: Az autók esetében az átlagosan eladott darabszám {Math.Round(osszesEladas,1)}\n";
-
-            /// 7. Feladat
-            lblMain.Content += ("7. feladat: Az elmúlt 5 évben gyártott autók:\n");
-
-            int currentYear = DateTime.Now.Year;
-
-            foreach (var item in Cars)
-            {
-                if (item.GyartasiEv > currentYear - 5)
-                {
-                    lblMain.Content += $"\t- {item.Marka} {item.Modell}: {item.GyartasiEv}\n";
-                }
-            }
         }
         public void ReadFile(string filePath)
         {
@@ -61,6 +44,40 @@ namespace VIZSGA_KOROM
             {
                 throw new ArgumentException();
             }
+        }
+
+        public void ConsoleFeladat()
+        {
+            /// 5. Feladat
+            lblMain.Content += $"5. Feladat: {Cars.Count} autó található a listában.\n";
+
+            /// 6. Feladat
+            double osszesEladas = Cars.Select(x => x.EladottDarabszam).Average();
+            lblMain.Content += $"6. Feladat: Az autók esetében az átlagosan eladott darabszám {Math.Round(osszesEladas, 1)}\n";
+
+            /// 7. Feladat
+            lblMain.Content += ("7. feladat: Az elmúlt 5 évben gyártott autók:\n");
+
+            int currentYear = DateTime.Now.Year;
+
+            foreach (var item in Cars)
+            {
+                if (item.GyartasiEv > currentYear - 5)
+                {
+                    lblMain.Content += $"\t- {item.Marka} {item.Modell}: {item.GyartasiEv}\n";
+                }
+            }
+
+            /// 8. Feladat
+            lblMain.Content += "8. feladat: Legsikeresebb márkák listája az eladott darabszám alapján:\n";
+
+            List<string> Manufacturers = Cars.Select(x=> x.Marka).Distinct().ToList();
+
+            foreach (var manufacturer in Manufacturers)
+            {
+                lblMain.Content += $"\t- {manufacturer}: {Cars.Where(x => x.Marka == manufacturer).Select(x => x.EladottDarabszam).Sum()} darab\n";
+            }
+
         }
     }
 }
